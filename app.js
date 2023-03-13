b3a553d364fc49b6677d4bacccb706347232c23a
dzechan98 <dedoanpro@gmail.com>
2023-03-13 17:39:38 +0700

    update files

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const productBox = document.querySelectorAll(".product-box");
 const cartContent = document.querySelector(".cart-content");
 var totolValue = document.querySelector(".totol-price");
 const btnBuy = document.querySelector(".btn-buy");
+const body = document.querySelector("body");
+const toast = document.querySelector(".toast");
 cartIcon.onclick = function () {
   cart.classList.add("active");
 };
@@ -12,6 +14,7 @@ closeCart.onclick = function () {
   cart.classList.remove("active");
 };
 
+btnBuy.addEventListener("click", handleSubmit);
 function removeCartItem(event) {
   var btnClick = event.target;
   btnClick.parentElement.remove();
@@ -31,64 +34,85 @@ function handleTotol() {
 
   totolValue.innerText = "$" + totol;
 }
+
+function handleAddItem(product, index) {
+  const cartBox = document.createElement("div");
+  cartBox.classList.add("cart-box");
+  cartBox.setAttribute("data-key", index);
+  const cartSrc = product.querySelector(".product-img").src;
+
+  const cartTitle = product.querySelector(".product-title").innerText;
+  const cartPrice = product.querySelector(".price").innerText;
+  cartBox.innerHTML = renderCartItem(cartSrc, cartTitle, cartPrice);
+  var isAdded = false;
+  var totol = 0;
+
+  //xu li them san pham vao gio hang
+  if (cartContent.querySelectorAll(".cart-box")) {
+    cartContent.querySelectorAll(".cart-box").forEach((item) => {
+      totol += handleTotol(item);
+      if (item.dataset.key == product.dataset.key) {
+        isAdded = true;
+      }
+    });
+  }
+  if (isAdded) {
+    alert("Sản phẩm đã được thêm vào giỏ hàng trước đó !!!");
+  } else {
+    cartContent.appendChild(cartBox);
+    //body.appendChild(toast);
+    //setTimeout(() => {
+    //   body.removeChild(toast);
+    // }, 6000);
+  }
+  handleTotol();
+}
+
+function renderCartItem(src, title, price) {
+  return `
+  <img
+  src="${src}"
+  alt=""
+  class="cart-img"
+/>
+<div class="detail-box">
+  <div class="cart-product-title">${title}</div>
+  <div class="cart-price">${price}</div>
+  <input type="number" value="1" min="1" class="cart-quantity" />
+</div>
+<i class="bx bxs-trash-alt cart-remove"></i>
+  `;
+}
+
 productBox.forEach((product, index) => {
   product.addEventListener("click", function () {
-    const cartBox = document.createElement("div");
-    cartBox.classList.add("cart-box");
-    cartBox.setAttribute("data-key", index);
-    const cartSrc = product.querySelector(".product-img").src;
-
-    const cartTitle = product.querySelector(".product-title").innerText;
-    const cartPrice = product.querySelector(".price").innerText;
-    cartBox.innerHTML = `
-      <img
-      src="${cartSrc}"
-      alt=""
-      class="cart-img"
-    />
-    <div class="detail-box">
-      <div class="cart-product-title">${cartTitle}</div>
-      <div class="cart-price">${cartPrice}</div>
-      <input type="number" value="1" min="1" class="cart-quantity" />
-    </div>
-    <i class="bx bxs-trash-alt cart-remove"></i>
-      `;
-    var isAdded = false;
-    var totol = 0;
-
-    //xu li them san pham vao gio hang
-    if (cartContent.querySelectorAll(".cart-box")) {
-      cartContent.querySelectorAll(".cart-box").forEach((item) => {
-        totol += handleTotol(item);
-        if (item.dataset.key == product.dataset.key) {
-          isAdded = true;
-        }
-      });
-    }
-    if (isAdded) {
-      alert("Sản phẩm đã được thêm vào giỏ hàng trước đó !!!");
-    } else {
-      cartContent.appendChild(cartBox);
-    }
-    handleTotol();
+    handleAddItem(product, index);
     //Xu li khi thay doi so luong san pham
-    const quantityProducts = document.querySelectorAll(".cart-quantity");
-    if (quantityProducts) {
-      quantityProducts.forEach((quantity) => {
-        quantity.addEventListener("input", handleTotol);
-      });
-    }
+    handleChangeInput();
     //Xu li xoa san pham khoi gio hang
-    const removeCartBtn = document.querySelectorAll(".cart-remove");
-    if (removeCartBtn) {
-      removeCartBtn.forEach((remove) => {
-        remove.addEventListener("click", removeCartItem);
-      });
-    }
+    handleDeleteItem();
   });
 });
+
+function handleChangeInput() {
+  const quantityProducts = document.querySelectorAll(".cart-quantity");
+  if (quantityProducts) {
+    quantityProducts.forEach((quantity) => {
+      quantity.addEventListener("input", handleTotol);
+    });
+  }
+}
+
+function handleDeleteItem() {
+  const removeCartBtn = document.querySelectorAll(".cart-remove");
+  if (removeCartBtn) {
+    removeCartBtn.forEach((remove) => {
+      remove.addEventListener("click", removeCartItem);
+    });
+  }
+}
 //xu li khi submit gio hang
-btnBuy.addEventListener("click", function () {
+function handleSubmit() {
   if (cartContent.querySelector(".cart-box") != null) {
     cartContent.querySelectorAll(".cart-box").forEach((item) => {
       item.remove();
@@ -98,4 +122,4 @@ btnBuy.addEventListener("click", function () {
   } else {
     alert("Giỏ hàng hiện đang trống");
   }
-});
+}
